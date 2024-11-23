7818d1ceea8a7f23d4d2b843a80d3343ebb8a5b2
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2024-11-23 13:33:58 +0100

    fix: update destructive badge styles for consistency

--- packages/frontend/components/ui/badge.tsx
+++ packages/frontend/components/ui/badge.tsx
@@ -12,8 +12,7 @@ const badgeVariants = cva(
 					'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
 				secondary:
 					'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-				destructive:
-					'border-transparent bg-destructive/10 text-destructive-foreground',
+				destructive: 'border-transparent bg-destructive/10 text-destructive',
 				outline: 'text-foreground',
 				primary: 'border-transparent bg-blue-500/10 text-blue-500'
 			}
