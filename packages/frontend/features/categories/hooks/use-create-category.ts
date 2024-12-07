cd2c9d114be374a156041f8520cb527ac716e1e8
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2024-12-07 13:43:35 +0100

    feat: create category hooks

--- packages/frontend/features/categories/hooks/use-create-category.ts
+++ packages/frontend/features/categories/hooks/use-create-category.ts
@@ -1,3 +1,4 @@
+import { useQueryClient } from '@tanstack/react-query'
 import { useTransition } from 'react'
 import { toast } from 'sonner'
 
@@ -5,14 +6,27 @@ import { createCategory } from '@/features/categories/actions'
 
 export default function useCreateCategory() {
 	const [isCreatingCategory, createCategoryTransition] = useTransition()
-	const onSubmit = (name: string) => {
+	const queryClient = useQueryClient()
+	const onSubmit = ({
+		name,
+		onClose
+	}: {
+		name: string
+		onClose?: () => void
+	}) => {
 		createCategoryTransition(async () => {
 			const category = await createCategory(name)
 			if (category && 'error' in category) {
 				toast.error('Error creando la categoría')
 				return
 			}
+			await queryClient.invalidateQueries({ queryKey: ['categories'] })
+			await queryClient.invalidateQueries({
+				queryKey: ['summary'],
+				exact: false
+			})
 			toast.success('Categoría creada')
+			onClose?.()
 		})
 	}
 	return { isCreatingCategory, onSubmit }
