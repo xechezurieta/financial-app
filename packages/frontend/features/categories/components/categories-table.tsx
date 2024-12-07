0e5c6d15f959d74b019dc4618a063c19811e485e
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2024-12-07 13:26:10 +0100

    feat: refactor CategoriesTable to use custom hook for deleting categories

--- packages/frontend/features/categories/components/categories-table.tsx
+++ packages/frontend/features/categories/components/categories-table.tsx
@@ -1,11 +1,7 @@
 'use client'
-import { Row } from '@tanstack/react-table'
-import { useTransition } from 'react'
-import { toast } from 'sonner'
-
 import { DataTable } from '@/components/table/data-table'
-import { deleteCategories } from '@/features/categories/actions'
 import { columns } from '@/features/categories/components/colums'
+import useDeleteCategories from '@/features/categories/hooks/use-delete-categories'
 import { Category } from '@/types/types'
 
 export default function CategoriesTable({
@@ -13,18 +9,7 @@ export default function CategoriesTable({
 }: {
 	categories: Category[]
 }) {
-	const [isDeleting, startDeleteTransition] = useTransition()
-	const handleDelete = (row: Row<Category>[]) => {
-		startDeleteTransition(async () => {
-			const ids = row.map((r) => r.original.id)
-			const data = await deleteCategories(ids)
-			if (data && 'error' in data) {
-				toast.error('Error eliminando categorías')
-				return
-			}
-			toast.success('Categorías eliminadas')
-		})
-	}
+	const { isDeleting, handleDelete } = useDeleteCategories()
 	return (
 		<DataTable
 			filterKey='name'
